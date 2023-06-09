0619f2433928a893fe4c51eea75aefca0effd5d1
maria <marianishanscs@gmail.com>
2023-06-09 04:04:56 -0700

    student-portal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+ import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import LogIn from './components/logIn';
+import { Routes, Route } from 'react-router-dom';
+import Details from './components/Details';
+import Errror from './components/Errror';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<LogIn />} />
+          <Route path='/details' element={<Details />} />
+          <Route path='*' element={<Errror />} />
+      </Routes>
+    </>
   );
 }
 
